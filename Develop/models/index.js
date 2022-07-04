2193356021f26cf7908d3fac1f5b9fb3310940b1
unknown <arestre@ncsu.edu>
2022-07-03 20:56:41 -0400

    set up CRUD for category routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,8 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(category, {
+  as: "category",
+  foreignKey: 'id'
+});
 
 // Categories have many Products
+Category.hasMany(products, {
+  foreignKey: "category_id"
+})
 
 // Products belongToMany Tags (through ProductTag)
 
